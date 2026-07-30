458f8850ceb2ca85725621b7bda0a17082fef344
Sakamoto, Kazunori <exkazuu@gmail.com>
2026-07-31 02:03:13 +0900

    chore: willboosterify this repo
    Applied @willbooster/wbfy v12.5.4.

--- .lefthook/post-merge/prepare.sh
+++ .lefthook/post-merge/prepare.sh
@@ -9,5 +9,6 @@ run_if_changed() {
 }
 
 run_if_changed "(mise\.toml|\.mise\.toml|\.tool-versions|\..+-version)" "mise install"
+if git diff --no-color -U0 ORIG_HEAD HEAD -- '*bunfig.toml' | grep --quiet -E '^[+-] *(globalStore|linker|publicHoistPattern)'; then rm -Rf -- 'e2e/fixture/node_modules' 'node_modules'; fi
 run_if_changed "(package\.json|bun\.lock|bunfig\.toml|\.npmrc|patches/)" "bun install"
 run_if_changed "(bunfig\.toml|\.npmrc)" "rm -Rf -- 'e2e/fixture/node_modules/.vite'"
