4d5b3c29a9bd2109aa951e28fb728078b5f99268
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2026-08-09 22:07:29 +0900

    chore: willboosterify this repo

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,13 +5,22 @@ const port = 3117;
 export default defineConfig({
   forbidOnly: !!process.env.CI,
   globalSetup: './test/helpers/globalSetup.ts',
-  projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }],
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+  ],
   reporter: process.env.CI ? 'github' : 'list',
   retries: process.env.CI ? 2 : 0,
   testDir: './test/e2e',
   use: {
+    screenshot: process.env.CI ? 'only-on-failure' : 'on',
+    video: process.env.CI ? 'on-first-retry' : 'retain-on-failure',
     baseURL: `http://localhost:${port}`,
-    trace: 'on-first-retry',
+    trace: process.env.CI ? 'on-first-retry' : 'retain-on-failure',
   },
   webServer: {
     command: `bun run dev -p ${port}`,
